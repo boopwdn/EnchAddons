a33a6a0c1f0547f273cb2a8019d224c86ddfc2f2
EnchStudio <enchstudio@outlook.com>
2025-03-12 13:07:07 +0800

    fix: tickCounter not increased

--- src/main/kotlin/net/skymoe/enchaddons/impl/mixincallback/MinecraftMixinCallback.kt
+++ src/main/kotlin/net/skymoe/enchaddons/impl/mixincallback/MinecraftMixinCallback.kt
@@ -3,11 +3,13 @@ package net.skymoe.enchaddons.impl.mixincallback
 import net.minecraft.client.multiplayer.WorldClient
 import net.skymoe.enchaddons.EA
 import net.skymoe.enchaddons.event.minecraft.MinecraftEvent
+import net.skymoe.enchaddons.util.tickCounter
 
 fun startGamePost() {
 }
 
 fun onRunTickPre() {
+    tickCounter++
     MinecraftEvent.Tick.Pre
         .also(EA.eventDispatcher)
 }
