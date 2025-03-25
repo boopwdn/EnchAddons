a814566af9a23dd197737112954fb8e396da0913
EnchStudio <enchstudio@outlook.com>
2025-03-25 12:24:26 +0800

    typo: ChatEvent Post to Pre

--- src/main/kotlin/net/skymoe/enchaddons/event/minecraft/ChatEvent.kt
+++ src/main/kotlin/net/skymoe/enchaddons/event/minecraft/ChatEvent.kt
@@ -5,9 +5,11 @@ import net.skymoe.enchaddons.event.Event
 sealed interface ChatEvent : Event {
     sealed interface Normal : ChatEvent {
         val message: String
+        val messageRaw: String
 
-        data class Post(
+        data class Pre(
             override val message: String,
+            override val messageRaw: String,
         ) : Normal
     }
 }
