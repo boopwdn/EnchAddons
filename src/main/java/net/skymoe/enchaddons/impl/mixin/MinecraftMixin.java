6ab6e24ffb2a0482d93c4de7b5231ce2881f28f6
EnchStudio <enchstudio@outlook.com>
2025-04-03 21:50:50 +0800

    refactor(General): rename onHideGUIPost method

--- src/main/java/net/skymoe/enchaddons/impl/mixin/MinecraftMixin.java
+++ src/main/java/net/skymoe/enchaddons/impl/mixin/MinecraftMixin.java
@@ -3,17 +3,12 @@ package net.skymoe.enchaddons.impl.mixin;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.settings.GameSettings;
-import net.skymoe.enchaddons.impl.EnchAddonsImpl;
-import net.skymoe.enchaddons.impl.EnchAddonsImplKt;
-import net.skymoe.enchaddons.impl.config.EnchAddonsConfig;
-import net.skymoe.enchaddons.impl.config.EnchAddonsConfigKt;
 import net.skymoe.enchaddons.impl.mixincallback.MinecraftMixinCallbackKt;
 import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
@@ -38,7 +33,7 @@ public class MinecraftMixin {
     }
 
     @Inject(method = "runTick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;hideGUI:Z", opcode = Opcodes.PUTFIELD, shift = At.Shift.AFTER))
-    private void runTickRedirect(CallbackInfo ci) {
+    private void onHideGUIPost(CallbackInfo ci) {
         MinecraftMixinCallbackKt.onHideGUIKeyDetect(gameSettings);
     }
 }
